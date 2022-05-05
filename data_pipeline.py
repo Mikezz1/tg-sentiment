92b7627e812cd80a70b8ee86e2a890a1844a7555
Михаил Олейник <mikhailoleynik@192.168.0.39>
2022-05-05 20:00:17 +0300

    initial commit

---
+++ data_pipeline.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from natasha import (
+    Segmenter,
+    NewsEmbedding,
+    NewsNERTagger,
+    Doc
+)
+
+if __name__ == '__main__':
+    
+    # read data
+    
+    # process data
+    
+    # save data
+    
+    pass
